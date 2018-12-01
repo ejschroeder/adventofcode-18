4e6f5afc1f3ef55a46fd4037cb990db9ee30a1ed
E.J. Schroeder <ej.schr@gmail.com>
2018-12-01 13:35:59 -0500

    Solve Day 1 - Chronal Calibration

---
+++ 1-chronal-calibration/soln.clj
@@ -0,0 +1,23 @@
+(require 
+  '[clojure.java.io :as io]
+  '[clojure.string :refer [split-lines]])
+
+(def nums 
+  (->>
+    (slurp "input.txt")
+    (split-lines)
+    (map #(Integer/parseInt %))))
+
+(defn first-duplicate-freq
+  ([nums] (first-duplicate-freq (cycle nums) 0 #{0}))
+  ([nums acc seen]
+    (let [freq (+ acc (first nums))]
+      (if (contains? seen freq)
+        freq
+        (recur (rest nums) freq (conj seen freq))))))
+
+(println
+  "Final Frequency:" (reduce + nums))
+
+(println
+  "First Duplicate Frequency:" (first-duplicate-freq nums))
